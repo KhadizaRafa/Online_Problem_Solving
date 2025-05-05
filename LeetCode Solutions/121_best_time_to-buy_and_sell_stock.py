822041b3df942754ca0c568177f1986975707e41
KhadizaRafa <khadiza301rafa@gmail.com>
2025-05-05 22:58:50 +0600

    Optimised code for leetcode problem - 1 and solved problem no 121

--- LeetCode Solutions/121_best_time_to-buy_and_sell_stock.py
+++ LeetCode Solutions/121_best_time_to-buy_and_sell_stock.py
@@ -1,60 +1,17 @@
 class Solution(object):
-    # def maxProfit(self, prices):
-    #     min_price = min(prices)
-    #     min_day= prices.index(min_price)
-    #     if min_day == len(prices) - 1:
-    #         return 0
-    #     p = prices[min_day+1:len(prices)]
-    #     profit = max(p) - min_price
-    #     if profit>0:
-    #         return profit
-    #     else:
-    #         return 0
-
-
-    # [7, 1, 5, 3, 6, 4]
     def maxProfit(self, prices):
-        buy_price = sell_price = 0
-        i = 0
-        j = len(prices)
-        while(i!=len(prices)):
-            profit = prices[j] - prices[i]
-            if profit>0:
-                if prices[i] < buy_price:
-                    buy_price = prices[i]
-                if prices[j] > sell_price:
-                    sell_price = prices[j]
-            j = j-1
-            if i == j:
-                i = i + 1
-                j = len(prices) - i
-
-        return profit
-        # min_price = prices[0]
-        # min_day = 0
-        # p = prices
-        # for index,price in enumerate(p):
-        #     profit = price-min_price
-        #     if profit<=min_price and index!=len(prices)-1:
-        #         min_price = price
-        #         p = p[index+1:len(prices)]
-        #     elif profit<=min_price and index==len(prices)-1:
-        #         p=p[index + 1:len(prices)]
-        # return min_price
-
-
-        # for index,price in enumerate(prices):
-        #
-        # min_price=min(prices)
-        # min_day=prices.index(min_price)
-        # if min_day == len(prices) - 1:
-        #     return 0
-        # p=prices[min_day + 1:len(prices)]
-        # profit=max(p) - min_price
-        # if profit > 0:
-        #     return profit
-        # else:
-        #     return 0
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
 
 res = Solution()
-print(res.maxProfit([7,1,5,3,6,4]))
+print(res.maxProfit([2,4,0,1]))
+
+# [7,1,5,3,6,4]
--- LeetCode Solutions/1_two_sum.py
+++ LeetCode Solutions/1_two_sum.py
@@ -1,19 +1,20 @@
 class Solution(object):
+    ################ Solution 1 ############
+    # def twoSum(self, nums, target):
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+
+    ################ Solution 2 ################# (better performance)
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        for index,num in enumerate(nums):
+            sub = target - num
+            if sub in nums and index!= nums.index(sub):
+                    return [index, nums.index(sub)]
 
 
-
-
-nums = [2,7,11,15]
-target = 9
+nums = [3,3]
+target = 6
 res = Solution()
 print(res.twoSum(nums,target))
