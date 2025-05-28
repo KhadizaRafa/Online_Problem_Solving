7fbb2be42a70fdedb3e8f50a48718fcf729065d4
KhadizaRafa <khadiza301rafa@gmail.com>
2025-05-28 16:53:17 +0600

    Solved 14 again and 88

--- LeetCode Solutions/14_longest_common_prefix.py
+++ LeetCode Solutions/14_longest_common_prefix.py
@@ -1,11 +1,8 @@
 class Solution:
     def longestCommonPrefix(self, strs: [str]) -> str:
 
+############# Solution 1 (Run time: 0 ms, Mem: 12.51 MB) ##########################
         shortest_str = strs[0]
-
-        # for str in strs:
-        #     if len(str) < len(shortest_str):
-        #         shortest_str = str
 
         for str in strs:
             while not str.startswith(shortest_str):
@@ -13,7 +10,23 @@ class Solution:
 
         return shortest_str
 
+############# Solution 2 (Run time: 7 ms, Mem: 12.53 MB)##########################
 
-strs = ["flow","reflower","flight"]
+        i=0
+        j=1
+        shortest_str=strs[0]
+        while j < len(strs):
+            if i < len(strs[j]) and i < len(shortest_str) and shortest_str[i] == strs[j][i]:
+                i=i + 1
+            else:
+                shortest_str=strs[j][0:i]
+                j=j + 1
+                i = 0
+
+        return shortest_str
+
+
+
+strs = ["dog","dracecar","dcar"]
 arr=Solution()
 print(arr.longestCommonPrefix(strs))
--- LeetCode Solutions/88_merge_sorted_array.py
+++ LeetCode Solutions/88_merge_sorted_array.py
@@ -1,26 +1,23 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        j = 0
-        for index,value in enumerate(nums1):
-            if value == 0 and j!=n:
-                nums1[index] = nums2[j]
-                j = j+1
+            i = m -1
+            j = n-1
+            k = m+n -1
+            while j>=0 :
+                if i>=0 and nums1[i]>nums2[j]:
+                    nums1[k]=nums1[i]
+                    i-=1
+                else:
+                    nums1[k]=nums2[j]
+                    j-=1
+                k-=1
 
-        nums1.sort()
 
 
-
-
-
-#
-# nums1 = []
-# m = 0
-# nums2 = [1]
-# n = 1
-nums1=[1, 2, 3, 0, 0, 0]
-m=3
-nums2=[2, 5, 6]
-n=3
+nums1=[2,0]
+m=1
+nums2=[1]
+n=1
 res=Solution()
 res.merge(nums1,m,nums2,n)
 print(nums1)
