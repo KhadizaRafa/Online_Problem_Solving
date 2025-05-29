4891c5450fdb6e062a2bd808825360fb8d0df672
KhadizaRafa <khadiza301rafa@gmail.com>
2025-05-29 12:00:02 +0600

    Solved problem 26 and tried a different approach for 169

--- LeetCode Solutions/169_majority_element.py
+++ LeetCode Solutions/169_majority_element.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums):
+############# Solution 1 Run-time: 19ms, Mem: 13.76 Mb #####################
         my_dictionary = {}
         for i in nums:
             if i not in my_dictionary.keys():
@@ -10,7 +11,25 @@ class Solution:
             if my_dictionary[i] > len(nums)/2:
                 return i
 
+############# Solution 2 (Bad solution) #####################
+        # count , max_val = 0,0
+        # for index, value in enumerate(nums):
+        #    if value == max_val:
+        #        continue
+        #    if nums.count(value) > count:
+        #        count =  nums.count(value)
+        #        max_val = value
+        #
+        # return max_val
 
-nums=[2, 2, 1, 1, 1, 2, 2]
+
+############# Solution 2 (Best solution by others) #####################
+
+        # nums.sort()
+        # # print(nums)
+        # # print(len(nums) // 2)
+        # return nums[len(nums) // 2]
+
+nums=[2, 2, 1, 1,3,3,3,3,3,3,3,3, 1, 2, 2]
 arr = Solution()
 print(arr.majorityElement(nums))
