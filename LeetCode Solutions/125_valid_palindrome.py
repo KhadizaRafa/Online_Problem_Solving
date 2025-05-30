39d0bf8804b7728109d011db0b32e559247449e7
KhadizaRafa <khadiza301rafa@gmail.com>
2025-05-30 10:26:19 +0600

    solved 125

--- LeetCode Solutions/125_valid_palindrome.py
+++ LeetCode Solutions/125_valid_palindrome.py
@@ -1,18 +1,19 @@
-def mySqrt(self, x):
-    """
-    :type x: int
-    :rtype: int
-    """
-    low=1
-    high=x
-    # mid=(low + high) // 2
-    while low < high:
-        mid=(low + high) // 2
-        res=mid * mid
-        if res > x:
-            high=mid - 1
-        elif res < x:
-            low=mid + 1
-        else:
-            return mid
-    return high
+import re
+class Solution(object):
+    def isPalindrome(self, s):
+########## Solution 1 ##################
+        # new_str = ''
+        # for i in s:
+        #     val = ord(i.lower())
+        #     if (val>=48 and val<=57) or (val >= 97 and val <= 122):
+        #         new_str+=i.lower()
+        #
+        # return new_str == new_str[::-1]
+
+
+########## Solution 2 ##################
+        pattern = "".join(re.findall(r'\b[0-9a-z]+\b',s.lower()))
+        return pattern == pattern[::-1]
+
+arr=Solution()
+print(arr.isPalindrome("A man, a plan, a canal: Panama"))
---
+++ LeetCode Solutions/15_3Sum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums = sorted(nums)
+        print(nums)
+
+arr=Solution()
+print(arr.threeSum([-1,0,1,2,-1,-4]))
