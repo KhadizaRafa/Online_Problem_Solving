76012d52669c0b37662302be9fdf8d96cc3c78ef
Khadiza Rafa <khadiza301rafa@gmail.com>
2025-10-14 22:46:28 +0600

    solving problem 16

--- LeetCode Solutions/Array_Problems/Two_Pointers/16_3Sum_closest.py
+++ LeetCode Solutions/Array_Problems/Two_Pointers/16_3Sum_closest.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def threeSum(self, nums,target):
         nums.sort()
+        diff= []
         print(nums)
         for i in range(len(nums)-2):
             left = i + 1
             right = len(nums)-1
-
             while(left<right):
                 print(nums[i] , nums[left], nums[right])
-                diff = target - (nums[i]+nums[left]+ nums[right])
+                diff.append(target - (nums[i]+nums[left]+ nums[right]))
+                if sum < target:
+                    left = left + 1
+                elif sum > target:
+                    right = right - 1
+
+
                 print(diff)
                 left = left + 1
                 right = right - 1
