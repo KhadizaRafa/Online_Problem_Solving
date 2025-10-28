5d23bf9bac50ab78a943d96a44962d0e1a93eab7
KhadizaRafa <khadiza301rafa@gmail.com>
2025-10-28 12:49:42 +0600

    Solved 26_remove_duplicated_problem

---
+++ LeetCode Solutions/Array_Problems/Two_Pointers/26_Remove_duplicates_from_sorted_array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+
+    def removeDuplicates(self, nums):
+        # shouldn't use pop in a loop
+        # for i in range(len(nums) - 1, 0, -1):
+        #     if nums[i] == nums[i - 1]:
+        #         nums.pop(i)
+        #
+        # return len(nums)
+        i = 1
+        j = 1
+
+        while(j!= len(nums)):
+            if nums[j] != nums[i -1]:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+        print(nums)
+        return len(nums[:i])
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+res = Solution()
+print(res.removeDuplicates(nums))
+
+# [0,1,2,2,3,3,4]
+# res.test(nums,m,[2,2],val)
